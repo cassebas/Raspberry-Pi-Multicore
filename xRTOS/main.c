503c4567d973642e6bf8e56dc5b7b9cf4f316d63
Caspar Treijtel <c.treijtel@xs4all.nl>
2020-01-15 10:51:18 +0100

    Implementation of offsets for alignment
    - For now a simple mechanism is used for setting the offsets of the
      slave cores.
    - Benchmarks are still hardcoded for now.

--- xRTOS/main.c
+++ xRTOS/main.c
@@ -114,11 +114,12 @@ void core0(void* pParam) {
 	int dir = 1;
     unsigned int cycles_begin, cycles_end, time;
     unsigned int iter=0;
+    unsigned int offset=0;
 	RegType_t ostick;
 	enable_counters();
 
-	// Initialize the array for the synthetic benchmark
-	volatile bigstruct_t mydata[SYNBENCH_DATASIZE];
+	/* // Initialize the array for the synthetic benchmark */
+	/* volatile bigstruct_t mydata[SYNBENCH_DATASIZE]; */
 
 	while (1) {
 		step += dir;
@@ -133,7 +134,7 @@ void core0(void* pParam) {
 
 		ostick = getOSTickCounter();
 		cycles_begin = read_counter();
-		// array_access_linear(mydata);
+		/* array_access_linear(mydata); */
 		bsort100_BubbleSort(Array1);
 		cycles_end = read_counter();
 		time = cycles_end - cycles_begin;
@@ -150,8 +151,8 @@ void core0(void* pParam) {
 		TextOut(Dc, 20, 80, &buf[0], strlen(&buf[0]));
 
 		log_info(0, buf,
-				 "Core 0 OSTick: %u Cycle count: %12u iteration: %u\n\r",
-				 ostick, time, ++iter);
+				 "Core 0 OSTick: %u Cycle count: %12u iteration: %u offset: %d\n\r",
+				 ostick, time, ++iter, offset);
 	}
 }
 
@@ -164,6 +165,7 @@ void core1(void* pParam) {
 	int dir = 1;
     unsigned int cycles_begin, cycles_end, time;
     unsigned int iter=0;
+    unsigned int offset=0;
 	RegType_t ostick;
 	enable_counters();
 
@@ -179,12 +181,12 @@ void core1(void* pParam) {
 		DoProgress(Dc, step, total, 10, 200, GetScreenWidth() - 20, 20, col);
 
 		/* This is core1, we are slave for the synchronization between cores */
-		sync_slave(0);
+		sync_slave(offset);
 
 		ostick = getOSTickCounter();
 		cycles_begin = read_counter();
-		// array_access_linear(mydata);
-		bsort100_BubbleSort(Array2);
+		array_access_linear(mydata);
+		/* bsort100_BubbleSort(Array2); */
 		cycles_end = read_counter();
 		time = cycles_end - cycles_begin;
 
@@ -200,8 +202,11 @@ void core1(void* pParam) {
 		TextOut(Dc, 20, 180, &buf[0], strlen(&buf[0]));
 
 		log_info(1, buf,
-				 "Core 1 OSTick: %u Cycle count: %12u iteration: %u\n\r",
-				 ostick, time, ++iter);
+				 "Core 1 OSTick: %u Cycle count: %12u iteration: %u offset: %d\n\r",
+				 ostick, time, ++iter, offset);
+
+		if (iter % 2000 == 0)
+			offset++;
 	}
 }
 
@@ -214,6 +219,7 @@ void core2(void* pParam) {
 	int dir = 1;
     unsigned int cycles_begin, cycles_end, time;
     unsigned int iter=0;
+    unsigned int offset=0;
 	RegType_t ostick;
 	enable_counters();
 
@@ -229,12 +235,12 @@ void core2(void* pParam) {
 		DoProgress(Dc, step, total, 10, 300, GetScreenWidth() - 20, 20, col);
 
 		/* This is core2, we are slave for the synchroniztion between cores */
-		sync_slave(0);
+		sync_slave(offset);
 
 		ostick = getOSTickCounter();
 		cycles_begin = read_counter();
-		// array_access_linear(mydata);
-		bsort100_BubbleSort(Array3);
+		array_access_linear(mydata);
+		/* bsort100_BubbleSort(Array3); */
 		cycles_end = read_counter();
 		time = cycles_end - cycles_begin;
 
@@ -250,8 +256,11 @@ void core2(void* pParam) {
 		TextOut(Dc, 20, 280, &buf[0], strlen(&buf[0]));
 
 		log_info(2, buf,
-				 "Core 2 OSTick: %u Cycle count: %12u iteration: %u\n\r",
-				 ostick, time, ++iter);
+				 "Core 2 OSTick: %u Cycle count: %12u iteration: %u offset: %d\n\r",
+				 ostick, time, ++iter, offset);
+
+		if (iter % 2000 == 0)
+			offset++;
 	}
 }
 
@@ -264,6 +273,7 @@ void core3(void* pParam) {
 	int dir = 1;
     unsigned int cycles_begin, cycles_end, time;
     unsigned int iter=0;
+    unsigned int offset=0;
 	RegType_t ostick;
 	enable_counters();
 
@@ -280,12 +290,12 @@ void core3(void* pParam) {
 		DoProgress(Dc, step, total, 10, 400, GetScreenWidth() - 20, 20, col);
 
 		/* This is core3, we are slave for the synchroniztion between cores */
-		sync_slave(0);
+		sync_slave(offset);
 
 		ostick = getOSTickCounter();
 		cycles_begin = read_counter();
-		// array_access_linear(mydata);
-		bsort100_BubbleSort(Array4);
+		array_access_linear(mydata);
+		/* bsort100_BubbleSort(Array4); */
 		cycles_end = read_counter();
 		time = cycles_end - cycles_begin;
 
@@ -301,8 +311,11 @@ void core3(void* pParam) {
 		TextOut(Dc, 20, 380, &buf[0], strlen(&buf[0]));
 
 		log_info(3, buf,
-				 "Core 3 OSTick: %u Cycle count: %12u iteration: %u\n\r",
-				 ostick, time, ++iter);
+				 "Core 3 OSTick: %u Cycle count: %12u iteration: %u offset: %d\n\r",
+				 ostick, time, ++iter, offset);
+
+		if (iter % 2000 == 0)
+			offset++;
 	}
 }
 
@@ -324,20 +337,18 @@ void main (void)
     unsigned int baudrate = 115200;
     if (pl011_uart_init(baudrate)) {
         printf("UART succesfully initialized\n");
-        log_info(0, buf, "%s\n\r", "UART succesfully initialized");
+        /* log_info(0, buf, "%s\n\r", "UART succesfully initialized"); */
     } else {
         printf("UART could not be initialized!\n");
-        log_warning(0, buf, "%s\n\r", "UART could not be initialized!");
     }
 
-    log_info(0, buf, "%s\n\r", "Initializing xRTOS");
 	xRTOS_Init();													// Initialize the xRTOS system .. done before any other xRTOS call
 
 	/* Initialize the bsort100 arrays */
 	bsort100_Initialize(Array1);
-	bsort100_Initialize(Array2);
-	bsort100_Initialize(Array3);
-	bsort100_Initialize(Array4);
+	/* bsort100_Initialize(Array2); */
+	/* bsort100_Initialize(Array3); */
+	/* bsort100_Initialize(Array4); */
 
 	/* Core 0 task */
 	xTaskCreate(0, core0, "Core0", 512, NULL, 2, NULL);
@@ -345,11 +356,11 @@ void main (void)
 	/* Core 1 task */
 	xTaskCreate(1, core1, "Core1", 512, NULL, 2, NULL);
 	
-	/* Core 2 task */
-	xTaskCreate(2, core2, "Core2", 512, NULL, 2, NULL);
+	/* /\* Core 2 task *\/ */
+	/* xTaskCreate(2, core2, "Core2", 512, NULL, 2, NULL); */
 
-	/* Core 3 task */
-	xTaskCreate(3, core3, "Core3", 512, NULL, 2, NULL);
+	/* /\* Core 3 task *\/ */
+	/* xTaskCreate(3, core3, "Core3", 512, NULL, 2, NULL); */
 
 	/* Start scheduler */
 	xTaskStartScheduler();
