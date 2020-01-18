bd68d56d94e7fe60c21defbe6297ae03628ba425
Caspar Treijtel <c.treijtel@xs4all.nl>
2020-01-18 15:06:45 +0100

    Use an extra field in the logs for easier selection

--- xRTOS/experiment/csv2summary.py
+++ xRTOS/experiment/csv2summary.py
@@ -3,6 +3,7 @@ from os.path import isdir, join
 import numpy as np
 import pandas as pd
 import glob
+from itertools import product
 
 
 benchmarks = ['malardalenbsort100',
@@ -30,8 +31,11 @@ def main(input_directory, output_directory):
             print('Processing input file {}.'.format(f))
             df = pd.read_csv(f)
 
+            # Construct a pivot table: benchmarks/core will be indexed as columns,
+            # to be able to easily select cycles from resulting CSV files within LaTeX.
+            #    ('cores' == nr of cores,  'core' == core number)
             pv = pd.pivot_table(df,
-                                index=['cores', 'pattern'],
+                                index=['cores', 'configuration', 'pattern'],
                                 columns=['benchmark', 'core'],
                                 values='cycles',
                                 aggfunc={'cycles': [np.median, np.std]})
@@ -43,45 +47,66 @@ def main(input_directory, output_directory):
         pvs.sort_index(inplace=True)
 
         # Now output a series of CVS files that contain the summaries
-        for bench in benchmarks:
-            for cores in range(1, 5):
-                cidx = ['core'+str(i) for i in range(cores)]
-                pvs_tmp = pvs.loc[cores, (slice(None), bench, cidx)]
-                pvs_tmp.dropna(how='any', inplace=True)
-                pvs_tmp.columns = pvs_tmp.columns.to_series().str.join('-')
-                # write to CVS output file
-                output_file = 'cycles-{}core-{}.csv'.format(cores, bench)
-                outfile = join(output_directory, output_file)
-                pvs_tmp.to_csv(outfile, index=True, sep=' ')
+        for cores in range(1, 5):
+            # A list of strings with the cores, like ['core0', 'core1', ..]
+            cidx = ['core'+str(i) for i in range(cores)]
+            for prod in product(benchmarks, repeat=cores):
+                # prod is a tuple with the names of the benchmarks, where each tuple in the
+                # iterator is member of the cartesian product of all benchmarks.
+                # Each tuple thus can represent a specific configuration of benchmarks running
+                # on cores in a specified order.
+                if len(set(prod)) == 1:
+                    # All the same benchmarks, simple case
+                    bench = prod[0]
+                    bench_idx = benchmarks.index(bench)
+                    pattern = '\'' + str(bench_idx) * cores + '\''
 
-        # For now combinations of benchmarks hard coded:
-        b1 = benchmarks[0]
-        b2 = benchmarks[1]
-        pvs2_0 = pvs.loc[2, (slice(None), [b1, b2], ['core0', 'core1'])]
-        pvs2_0.dropna(how='all', inplace=True)
+                    # First construct the complete pivot table where this specific benchmark,
+                    # all patterns and wanted cores (cidx) are listed.
+                    if pattern in pvs.index.get_level_values(1):
+                        pvs_tmp = pvs.loc[(cores, pattern, slice(None)),
+                                          (slice(None), bench, cidx)]
+                        pvs_tmp.dropna(how='any', inplace=True)
+                        pvs_tmp.columns = pvs_tmp.columns.to_series().str.join('-')
+                        # write to CVS output file
+                        output_file = 'cycles-{}core-{}.csv'.format(cores, bench)
+                        outfile = join(output_directory, output_file)
+                        pvs_tmp.to_csv(outfile, index=True, sep=' ')
+                else:
+                    # Not all the same benchmarks: we have to do some tricks to make sure
+                    # the pivot table contains only the rows that are relevant to the exact
+                    # (ordered) combination of benchmarks running on specific cores.
+                    bench_list = list(prod)
+                    print('\n\n\n{}'.format(bench_list))
+                    pattern = '\''
+                    for i in range(cores):
+                        bench_idx = benchmarks.index(bench_list[i])
+                        pattern += str(bench_idx)
+                    pattern += '\''
 
-        pvs2_1 = pvs2_0.loc[:, (slice(None), b1, ['core0', 'core1'])].dropna(how='all')
-        # Remove rows that are complete, these aren't combinations of
-        # benchmarks but consist of the same benchmark on each core:
-        pvs2_1 = pvs2_1[pvs2_1.isnull().any(1)]
-        # Get the correct slice, 1st benchmark runs on core0
-        pvs2_1 = pvs2_1.loc[:, (slice(None), b1, 'core0')]
+                    # pvs_cb: selection of pvs with all prod's benchmarks and cores in cidx:
+                    if pattern in pvs.index.get_level_values(1):
+                        pvs_cb = pvs.loc[(cores, pattern, slice(None)),
+                                         (slice(None), bench_list[0], cidx[0])]
+                        for i in range(1, cores):
+                            pvs_tmp = pvs.loc[(cores, pattern, slice(None)),
+                                              (slice(None), bench_list[i], cidx[i])]
+                            pvs_cb = pd.merge(pvs_cb, pvs_tmp,
+                                              left_index=True,
+                                              right_index=True)
+                        pvs_cb.dropna(how='any', inplace=True)
 
-        pvs2_2 = pvs2_0.loc[:, (slice(None), b2, ['core0', 'core1'])].dropna(how='all')
-        # Remove rows that are complete, these aren't combinations of
-        # benchmarks but consist of the same benchmark on each core:
-        pvs2_2 = pvs2_2[pvs2_2.isnull().any(1)]
-        # Get the correct slice, 2nd benchmark runs on core1
-        pvs2_2 = pvs2_2.loc[:, (slice(None), b2, 'core1')]
-
-        pvs_merge = pd.merge(pvs2_1, pvs2_2, left_index=True, right_index=True)
-        pvs_merge.columns = pvs_merge.columns.to_series().str.join('-')
-
-        output_file = 'cycles-{}core-{}-{}.csv'.format(2,
-                                                       benchmarks[0],
-                                                       benchmarks[1])
-        outfile = join(output_directory, output_file)
-        pvs_merge.to_csv(outfile, index=True, sep=' ')
+                        if len(pvs_cb.index) > 0:
+                            # We have results for this configuration!
+                            pvs_cb.columns = pvs_cb.columns.to_series().str.join('-')
+                            bench_str = '-'.join(prod)
+                            output_file = ('cycles-{}core-'.format(cores) +
+                                           bench_str + '.csv')
+                            outfile = join(output_directory, output_file)
+                            pvs_cb.to_csv(outfile, index=True, sep=' ')
+                    else:
+                        print('This combination of benchmarks was not run:',
+                              ' '.join(prod))
 
     else:
         print('Warning: directory {}'.format(input_directory), end=' ')
--- xRTOS/experiment/log2csv.awk
+++ xRTOS/experiment/log2csv.awk
@@ -1,43 +1,38 @@
 BEGIN {
-    print "benchmark,cores,pattern,core,cycles,iteration,offset"
+    print "configuration,benchmark,cores,pattern,core,cycles,iteration,offset"
 }
 {
-    if (length($15) > 0) {
+    if (length($17) > 0) {
         # remove underscores from benchmark name, it hurts LaTeX
-        gsub("_", "", $4)
+        gsub("_", "", $6)
 
-        if ($6 == 1) {
+        if ($8 == 1) {
             /* 1 core */
-            if ($15 <= 2000)
-                printf("%s,%s,'0',%s,%s,%s,%s\n",$4,$6,$8,$13,$15,$17)
-            else if ($15 <= 4000)
-                printf("%s,%s,'0',%s,%s,%s,%s\n",$4,$6,$8,$13,$15,$17)
-            else if ($15 <= 6000)
-                printf("%s,%s,'0',%s,%s,%s,%s\n",$4,$6,$8,$13,$15,$17)
-         } else if ($6 == 2) {
+            printf("%s,%s,%s,'0',%s,%s,%s,%s\n",$4,$6,$8,$10,$15,$17,$19)
+         } else if ($8 == 2) {
             /* 2 cores */
-            if ($15 <= 2000)
-                printf("%s,%s,'00',%s,%s,%s,%s\n",$4,$6,$8,$13,$15,$17)
-            else if ($15 <= 4000)
-                printf("%s,%s,'01',%s,%s,%s,%s\n",$4,$6,$8,$13,$15,$17)
-            else if ($15 <= 6000)
-                printf("%s,%s,'02',%s,%s,%s,%s\n",$4,$6,$8,$13,$15,$17)
-        } else if ($6 == 3) {
+            if ($17 <= 2000)
+                printf("%s,%s,%s,'00',%s,%s,%s,%s\n",$4,$6,$8,$10,$15,$17,$19)
+            else if ($17 <= 4000)
+                printf("%s,%s,%s,'01',%s,%s,%s,%s\n",$4,$6,$8,$10,$15,$17,$19)
+            else if ($17 <= 6000)
+                printf("%s,%s,%s,'02',%s,%s,%s,%s\n",$4,$6,$8,$10,$15,$17,$19)
+        } else if ($8 == 3) {
             /* 3 cores */
-            if ($15 <= 2000)
-                printf("%s,%s,'000',%s,%s,%s,%s\n",$4,$6,$8,$13,$15,$17)
-            else if ($15 <= 4000)
-                printf("%s,%s,'011',%s,%s,%s,%s\n",$4,$6,$8,$13,$15,$17)
-            else if ($15 <= 6000)
-                printf("%s,%s,'022',%s,%s,%s,%s\n",$4,$6,$8,$13,$15,$17)
+            if ($17 <= 2000)
+                printf("%s,%s,%s,'000',%s,%s,%s,%s\n",$4,$6,$8,$10,$15,$17,$19)
+            else if ($17 <= 4000)
+                printf("%s,%s,%s,'011',%s,%s,%s,%s\n",$4,$6,$8,$10,$15,$17,$19)
+            else if ($17 <= 6000)
+                printf("%s,%s,%s,'022',%s,%s,%s,%s\n",$4,$6,$8,$10,$15,$17,$19)
         } else {
             /* 4 cores */
-            if ($15 <= 2000)
-                printf("%s,%s,'0000',%s,%s,%s,%s\n",$4,$6,$8,$13,$15,$17)
-            else if ($15 <= 4000)
-                printf("%s,%s,'0111',%s,%s,%s,%s\n",$4,$6,$8,$13,$15,$17)
-            else if ($15 <= 6000)
-                printf("%s,%s,'0222',%s,%s,%s,%s\n",$4,$6,$8,$13,$15,$17)
+            if ($17 <= 2000)
+                printf("%s,%s,%s,'0000',%s,%s,%s,%s\n",$4,$6,$8,$10,$15,$17,$19)
+            else if ($17 <= 4000)
+                printf("%s,%s,%s,'0111',%s,%s,%s,%s\n",$4,$6,$8,$10,$15,$17,$19)
+            else if ($17 <= 6000)
+                printf("%s,%s,%s,'0222',%s,%s,%s,%s\n",$4,$6,$8,$10,$15,$17,$19)
         }
    }
 }
