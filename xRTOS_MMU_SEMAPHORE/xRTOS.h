19a0bc3d9310f27dd28bd3f60587a2d6ebccea96
Caspar Treijtel <c.treijtel@xs4all.nl>
2020-05-03 20:13:29 +0200

    Increase frequency of timer interrupts to 10kHz

--- xRTOS_MMU_SEMAPHORE/xRTOS.h
+++ xRTOS_MMU_SEMAPHORE/xRTOS.h
@@ -3,7 +3,11 @@
 
 #define MAX_CPU_CORES							( 4	)				// The Raspberry Pi3 has 4 cores	
 #define MAX_TASKS_PER_CORE						( 8 )				// For the moment task storage is static so we need some size
-#define configTICK_RATE_HZ						( 1000 )			// Timer tick frequency	
+/**
+ * CT set Timer tick frequency at 10kHz, effectively setting interrupts at every
+ * 120,000 cylces instead of 1,200,000 cycles.
+ */
+#define configTICK_RATE_HZ						( 10000 )			// Timer tick frequency	
 #define tskIDLE_PRIORITY						( 0	)				// Idle priority is 0 .. rarely would this ever change	
 #define configMAX_TASK_NAME_LEN					( 16 )				// Maxium length of a task name
 #define configMINIMAL_STACK_SIZE				( 128 )				// Minimum stack size used by idle task
